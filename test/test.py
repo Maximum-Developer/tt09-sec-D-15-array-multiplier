682e62196e76270ad1605dcc584375d7cf989db4
Maximum-Developer <146565485+Maximum-Developer@users.noreply.github.com>
2024-10-25 10:58:54 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -45,14 +45,14 @@ async def test_project(dut):
     # Change it to match the actual expected output of your module:
     assert dut.uo_out.value == 0
 
-    dut.ui_in.value = 0b00010000 
+    dut.ui_in.value = 0b00010011 
 
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 1)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 1
+    assert dut.uo_out.value == 3
 
     dut.ui_in.value = 0b10100011 
 
